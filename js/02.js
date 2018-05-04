dc8f028e0b830c264f812f2b408e1e88e8a3d935
yangdezhicy <yangdezhicy@163.com>
2018-05-04 20:30:19 +0800

    yangdezhi11

--- js/02.js
+++ js/02.js
@@ -28,21 +28,21 @@ $().ready(function(){
 			}else{
 				$(".navbar").css("background","none");
 			}
-			if(top >= 0&&top<862){
+			if(top >= 0&&top<949){
 				console.log("home");
 				tap("#a1","#a2","#a3","#a4","#a5");
-			}else if(top >= 862&&top<1539){
+			}else if(top >= 949&&top<1631){
 				console.log("about");
 				tap("#a2","#a1","#a3","#a4","#a5");
 			}
-			else if(top > 1539&&top<2229){
+			else if(top > 1631&&top<2390){
 				console.log("service");
 				tap("#a3","#a2","#a1","#a4","#a5");
 			}
-			else if(top >= 2229&&top<2819){
+			else if(top >= 2390&&top<2819){
 				console.log("experience");
 				tap("#a4","#a2","#a3","#a1","#a5");
-			}else if(top >= 2819){
+			}else if(top >= 2902){
 				console.log("education");
 				tap("#a5","#a2","#a3","#a4","#a1");
 			}
