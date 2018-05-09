0bb8e993d29cf97535ad86605f5ceb8a07fbacda
yangdezhicy <yangdezhicy@163.com>
2018-05-09 19:49:19 +0800

    Three

--- js/02.js
+++ js/02.js
@@ -13,35 +13,6 @@ $().ready(function(){
 				return false;
 			})
 		}
-		function tap(el01,el02,el03,el04,el05){
-				$(el01).css("color","#E3AE57");
-				$(el02).css("color","#ccc");
-				$(el03).css("color","#ccc");
-				$(el04).css("color","#ccc");
-				$(el05).css("color","#ccc");
-			}
-		$(window).scroll(function() {
-			var top = $(document).scrollTop();
-			// console.log(top);
-			if(top > 0){
-				$(".navbar").css("background-color","#fff");
-			}else{
-				$(".navbar").css("background","none");
-			}
-			if(top >= 0&&top<842){
-				tap("#a1","#a2","#a3","#a4","#a5");
-			}else if(top >= 842&&top<1481){
-				tap("#a2","#a1","#a3","#a4","#a5");
-			}
-			else if(top > 1481&&top<1800){
-				tap("#a3","#a2","#a1","#a4","#a5");
-			}
-			else if(top >= 1800&&top<2300){
-				tap("#a4","#a2","#a3","#a1","#a5");
-			}else if(top >= 2300){
-				tap("#a5","#a2","#a3","#a4","#a1");
-			}
-		})
 	});
 	if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
 		new WOW().init();
